33e3b18a4108c4f49387ac1b6859c4ff0b546bb3
benjiramm <benjiramm@gmail.com>
2023-08-12 13:37:53 +0300

    started working on adding new shift

--- components/new_shift/AddStaffForShift.tsx
+++ components/new_shift/AddStaffForShift.tsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 import styles from "../../styles/new_shift.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NewEmptyStaff from "./NewEmptyStaff";
+import { useSelector } from "react-redux";
+import { newStaffType } from "@/slices/newShiftSlice";
+import NewStaffSales from "./NewStaffSales";
 
 const AddStaffForShift = () => {
   const [newEmptyStaff, setNewEmptyStaff] = useState<boolean>(true);
 
+  const staff: Array<newStaffType> = useSelector(
+    (state: any) => state.new_shift.staff
+  );
+
+  console.log(staff);
   return (
     <div className={styles.staff_display}>
       <div className={styles.add_staff_title}>צוות</div>
       {/** staff from context */}
+      {staff.map((s) => (
+        <NewStaffSales /> // s is for staff, has the data inside
+      ))}
       {/** empty new staff - depending on state */}
       {newEmptyStaff ? (
         <NewEmptyStaff deleteFunction={() => setNewEmptyStaff(false)} />
