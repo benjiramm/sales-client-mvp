6dcb9ed344da30b27f9b0c7e8c58fa7131aaf178
benjiramm <benjiramm@gmail.com>
2023-08-28 20:30:47 +0300

    styled and added content

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -38,14 +38,59 @@ const Navbar = () => {
         >
           <ul>
             <li>
-              <Link href={"#"}>Home</Link>
+              <Link href={"/"}>
+                <FontAwesomeIcon icon="home" /> בית
+              </Link>
+            </li>
+            {/* AUTH LINKS */}
+            {user && (
+              <>
+                <li>
+                  <Link href={"/protected/new_shift"}>
+                    <FontAwesomeIcon icon="circle-plus" /> הכנס/י מכירות
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/protected/history"}>
+                    <FontAwesomeIcon icon="calendar-alt" /> היסטוריה
+                  </Link>
+                </li>
+              </>
+            )}
+
+            {/* PUBLIC LINKS */}
+            <li>
+              <Link href={"items"}>
+                <FontAwesomeIcon icon="list" /> פריטים
+              </Link>
             </li>
             <li>
-              <Link href={"#"}>About</Link>
+              <Link href={"staff"}>
+                <FontAwesomeIcon icon="users" /> צוות
+              </Link>
             </li>
-            <li>
-              <Link href={"#"}>Contacts</Link>
-            </li>
+            {/* SUPERUSER LINKS*/}
+            {user?.is_admin && (
+              <li>
+                <Link href={"/protected/users"}>
+                  <FontAwesomeIcon icon="user-tie" /> מנהלים
+                </Link>
+              </li>
+            )}
+            {/* LOGIN/LOGOUT LINKS */}
+            {user ? (
+              <li>
+                <Link href={"/"} onClick={() => handleLogout()}>
+                  <FontAwesomeIcon icon="right-from-bracket" /> התנתק/י
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link href={"login"}>
+                  <FontAwesomeIcon icon="right-to-bracket" /> התחברי/י
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </div>
