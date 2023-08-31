511928e62799593972387fe92f4077329d415043
benjiramm <benjiramm@gmail.com>
2023-08-31 22:55:53 +0300

    changed middleware api endpoint

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
   if (!token) {
-    return NextResponse.redirect("http://localhost:3001/login");
+    return NextResponse.redirect(`${process.env.API}/login`);
   }
   return NextResponse.next();
 }
