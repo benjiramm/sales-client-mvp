b08466548d98970fd9ac89e78f8b8d065e279c43
benjiramm <benjiramm@gmail.com>
2023-08-31 23:16:12 +0300

    reverted to backend from heroku

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = nextConfig
 module.exports = {
   reactStrictMode: true,
   env: {
-    //API: 'https://sales-app-v2-server-d4fa7347984d.herokuapp.com',
-    API: 'http://localhost:3000'
+    API: 'https://sales-app-v2-server-d4fa7347984d.herokuapp.com',
+    //API: 'http://localhost:3000'
   }
 }
