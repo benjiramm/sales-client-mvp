4c295f924303352b2595ca52fae1d79a702ac72d
benjiramm <benjiramm@gmail.com>
2023-09-02 15:49:52 +0300

    fixed to the right api

--- next.config.js
+++ next.config.js
@@ -8,6 +8,6 @@ module.exports = nextConfig
 module.exports = {
   reactStrictMode: true,
   env: {
-    API: process.env.NODE_ENV =='development' ? 'https://www.api.sellingcompetition.com':'http://localhost:3000',
+    API: process.env.NODE_ENV =='development' ? 'http://localhost:3000':'https://www.api.sellingcompetition.com',
   }
 }
