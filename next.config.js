a78ce892c20535ce9124d0e167eb2f2e90c2a81b
benjiramm <benjiramm@gmail.com>
2023-09-02 11:47:26 +0300

    removed strict mode

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
 module.exports = nextConfig
 
 module.exports = {
-  reactStrictMode: true,
   env: {
     API: 'https://sales-app-v2-server-d4fa7347984d.herokuapp.com',
     //API: 'http://localhost:3000'
