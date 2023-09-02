07cc61d4f49ef67439076b960ca4e986b5658c45
benjiramm <benjiramm@gmail.com>
2023-09-02 13:45:28 +0300

    added reactstrictmode

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
 module.exports = nextConfig
 
 module.exports = {
+  reactStrictMode: true,
   env: {
     API: 'http://www.api.sellingcompetition.com',
     //API: 'http://localhost:3000'
